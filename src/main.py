ed3fd9e97cc4039b076dd24fbc6542443f6d1fcc
~Jon <jhealy278@gmail.com>
2021-11-28 18:20:12 -0500

    Read file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import os.path
+
+attributes = []
+month_averages = {}
 
 
 def average():  # TODO: Average out data
@@ -8,17 +12,23 @@ def categorize():  # TODO: Separate data into each category
     print("TODO")
 
 
-def process_line():  # TODO: process Each line
-    print("TODO")
+def process_line(line: str):  # TODO: process Each line
+    data = line.split(',')
+    print(data)
 
 
-def read_file():  # TODO: Read file from lib
-    print("TODO")
-
-
-def main():
-    print("Main function")
+def read_file():
+    data = False
+    with open(os.getcwd() + os.path.sep + '..' + os.path.sep + 'lib' + os.path.sep + 'out.arff') as file:
+        for line in file:
+            if '@data' in line:
+                data = True
+            elif '@attribute' in line:
+                attributes.append(line.split(' ')[1].strip("'"))
+            elif data:
+                process_line(line.strip())
+        print(attributes)
 
 
 if __name__ == '__main__':
-    main()
+    read_file()
