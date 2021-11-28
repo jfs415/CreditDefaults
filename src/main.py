dc312c40d9e4674bd954390d9ddaa277335c1ed7
~Jon <jhealy278@gmail.com>
2021-11-28 16:25:41 -0500

    Framework

---
+++ src/main.py
@@ -0,0 +1,24 @@
+
+
+def average():  # TODO: Average out data
+    print("TODO")
+
+
+def categorize():  # TODO: Separate data into each category
+    print("TODO")
+
+
+def process_line():  # TODO: process Each line
+    print("TODO")
+
+
+def read_file():  # TODO: Read file from lib
+    print("TODO")
+
+
+def main():
+    print("Main function")
+
+
+if __name__ == '__main__':
+    main()
