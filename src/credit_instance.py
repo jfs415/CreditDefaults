7198e1c40e21f941bef694d9a5c5086f679cd5cf
~Jon <jhealy278@gmail.com>
2021-11-28 23:40:42 -0500

    More framework

--- src/credit_instance.py
+++ src/credit_instance.py
@@ -1,14 +1,19 @@
 class CreditInstance:
+    month = None
     pay = None
     bill_amount = None
     pay_amount = None
     defaulted = None
 
-    def __init__(self, pay: str, bill_amount: int, pay_amount: int, defaulted: bool):
+    def __init__(self, month: int, pay: str, bill_amount: int, pay_amount: int, defaulted: bool):
+        self.month = month
         self.pay = pay
         self.bill_amount = bill_amount
         self.pay_amount = pay_amount
         self.defaulted = defaulted
+
+    def get_month(self):
+        return self.month
 
     def get_pay(self):
         return self.pay
--- src/main.py
+++ src/main.py
@@ -1,20 +1,216 @@
 import os.path
+from typing import List
 
-attributes = []
-month_averages = {}
+months = {
+    '1': {
+        'sex': {
+            'male': [],
+            'female': []
+        },
+        'education': {
+            'graduate_school': [],
+            'university': [],
+            'high_school': [],
+            'other': []
+        },
+        'marriage': {
+            'married': [],
+            'single': [],
+            'other': []
+        },
+        'balance_limit': {
+            '1000_9999': [],
+            '10000_49999': [],
+            '50000_249999': [],
+            '250000_1000000': []
+        },
+        'age': {
+            '18_25': [],
+            '26_39': [],
+            '40_64': [],
+            '65+': []
+        }
+    },
+    '2': {
+        'sex': {
+            'male': [],
+            'female': []
+        },
+        'education': {
+            'graduate_school': [],
+            'university': [],
+            'high_school': [],
+            'other': []
+        },
+        'marriage': {
+            'married': [],
+            'single': [],
+            'other': []
+        },
+        'balance_limit': {
+            '1000_9999': [],
+            '10000_49999': [],
+            '50000_249999': [],
+            '250000_1000000': []
+        },
+        'age': {
+            '18_25': [],
+            '26_39': [],
+            '40_64': [],
+            '65+': []
+        }
+    },
+    '3': {
+        'sex': {
+            'male': [],
+            'female': []
+        },
+        'education': {
+            'graduate_school': [],
+            'university': [],
+            'high_school': [],
+            'other': []
+        },
+        'marriage': {
+            'married': [],
+            'single': [],
+            'other': []
+        },
+        'balance_limit': {
+            '1000_9999': [],
+            '10000_49999': [],
+            '50000_249999': [],
+            '250000_1000000': []
+        },
+        'age': {
+            '18_25': [],
+            '26_39': [],
+            '40_64': [],
+            '65+': []
+        }
+    },
+    '4': {
+        'sex': {
+            'male': [],
+            'female': []
+        },
+        'education': {
+            'graduate_school': [],
+            'university': [],
+            'high_school': [],
+            'other': []
+        },
+        'marriage': {
+            'married': [],
+            'single': [],
+            'other': []
+        },
+        'balance_limit': {
+            '1000_9999': [],
+            '10000_49999': [],
+            '50000_249999': [],
+            '250000_1000000': []
+        },
+        'age': {
+            '18_25': [],
+            '26_39': [],
+            '40_64': [],
+            '65+': []
+        }
+    },
+    '5': {
+        'sex': {
+            'male': [],
+            'female': []
+        },
+        'education': {
+            'graduate_school': [],
+            'university': [],
+            'high_school': [],
+            'other': []
+        },
+        'marriage': {
+            'married': [],
+            'single': [],
+            'other': []
+        },
+        'balance_limit': [],
+        'age': {
+            '18_25': [],
+            '26_39': [],
+            '40_64': [],
+            '65+': []
+        }
+    },
+    '6': {
+        'sex': {
+            'male': [],
+            'female': []
+        },
+        'education': {
+            'graduate_school': [],
+            'university': [],
+            'high_school': [],
+            'other': []
+        },
+        'marriage': {
+            'married': [],
+            'single': [],
+            'other': []
+        },
+        'balance_limit': {
+            '1000_9999': [],
+            '10000_49999': [],
+            '50000_249999': [],
+            '250000_1000000': []
+        },
+        'age': {
+            '18_25': [],
+            '26_39': [],
+            '40_64': [],
+            '65+': []
+        }
+    }
+}
+
+
+def is_outlier(data: List[str]):
+    pay_type_count = 0
+    for pay_type in (data[6:11]):  # Check if there was no consumption more than 4 times
+        if 'no_consumption' in pay_type:
+            pay_type_count += 1
+
+    if pay_type_count > 4:
+        return True
+
+    bill_count = 0
+    for bill in data[12:17]:  # Check if bill was 0 more than 4 times
+        if int(bill) == 0:
+            bill_count += 1
+
+    pay_count = 0
+    for pay in data[18:23]:  # Check if more than 4 payments were also $0
+        if int(pay) == 0:
+            pay_count += 1
+
+    if bill_count > 4 and pay_count > 4:  # If bill amount and pay amount were $0 more than 4 times it's an outlier
+        return True
+
+    return False
 
 
 def average():  # TODO: Average out data
     print("TODO")
 
 
-def categorize():  # TODO: Separate data into each category
-    print("TODO")
-
-
-def process_line(line: str):  # TODO: process Each line
-    data = line.split(',')
+def categorize(data: List[str]):  # TODO: Separate data into each category
     print(data)
+
+
+def process_line(line: str):  # Process each line
+    data = line.split(',')
+    if not is_outlier(data):
+        categorize(data)
 
 
 def read_file():
@@ -23,11 +219,8 @@ def read_file():
         for line in file:
             if '@data' in line:
                 data = True
-            elif '@attribute' in line:
-                attributes.append(line.split(' ')[1].strip("'"))
             elif data:
                 process_line(line.strip())
-        print(attributes)
 
 
 if __name__ == '__main__':
